799d8fb865f1907fc71dd93620edcee6bf7b94fa
syoki.isihira <zenkoku33@gmail.com>
2021-12-22 20:40:53 +0900

    会員登録画面＆ログイン画面

--- Views/home.php
+++ Views/home.php
@@ -99,9 +99,16 @@ function convertToDayTimeAgo(string $datetime)
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" href="<?php echo HOME_URL; ?>Views/img/logo-twitterblue.svg">
+    <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
     <link rel= "stylesheet" href="<?php echo HOME_URL; ?>Views/css/style.css">
- 
+    <!-- JS -->
+    <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous" defer></script>
+    <!-- JavaScript Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous" defer></script>
+    <!-- いいね！JS -->
+    <script src="<?php echo HOME_URL; ?>Views/js/likes.js" defer></script>
+
     <title>ホーム画面 / Twitterクローン</title>
     <meta name="description" content="ホーム画面です">
 </head>
@@ -116,7 +123,10 @@ function convertToDayTimeAgo(string $datetime)
                     <li class="nav-item"><a href="notification.php" class="nav-link"><img src="<?php echo HOME_URL; ?>Views/img/icon-notification.svg" alt=""></a></li>
                     <li class="nav-item"><a href="profile.php" class="nav-link"><img src="<?php echo HOME_URL; ?>Views/img/icon-profile.svg" alt=""></a></li>
                     <li class="nav-item"><a href="post.php" class="nav-link"><img src="<?php echo HOME_URL; ?>Views/img/icon-post-tweet-twitterblue.svg" alt=""class="post-tweet"></a></li>
-                    <li class="nav-item my-icon"><img src="<?php echo HOME_URL; ?>Views/img_uploaded/user/sample-person.jpg" alt=""></li>
+                    <li class="nav-item my-icon"><img src="<?php echo HOME_URL; ?>Views/img_uploaded/user/sample-person.jpg" alt="" class="js-popover"
+                     data-bs-container="body" data-bs-toggle="popover" data-bs-placement="right" data-bs-html="true"
+                     data-bs-content="<a href='profile.php'>プロフィール</a><br><a href='sign-out.php'>ログアウト<a/>"
+                    ></li>
                 </ul>
             </div>
         </div>
@@ -166,11 +176,11 @@ function convertToDayTimeAgo(string $datetime)
                         <p><?php echo $view_tweet['tweet_body'] ?></p>
 
                         <?php if (isset($view_tweet['tweet_image_name'])) : ?>
-                            <img src="<?php echo buildImagePath($view_tweet['tweet_image_name'],'tweet'); ?>" alt="" class="post_image">
+                            <img src="<?php echo buildImagePath($view_tweet['tweet_image_name'],'tweet'); ?>" alt="" class="post-image">
                         <?php endif; ?>
 
                         <div class="icon-list">
-                            <div class="like">
+                            <div class="like js-like" data-lile-id="<?php echo htmlspecialchars($views_tweet['like_id']); ?>">
                                 <?php
                                 if(isset($view_tweet['like_id'])) {
                                     // いいね！している場合、青のハートを表示
@@ -182,7 +192,7 @@ function convertToDayTimeAgo(string $datetime)
                                 ?>
                                 
                             </div>
-                            <div class="like-count"><?php echo htmlspecialchars($view_tweet['like_count']); ?></div>
+                            <div class="like-count js-like-count"><?php echo htmlspecialchars($view_tweet['like_count']); ?></div>
                         </div>
                     </div>
                 </div>
@@ -191,5 +201,10 @@ function convertToDayTimeAgo(string $datetime)
             <?php endif; ?>
         </div>
     </div> 
+    <script>
+        document.addEventListener('DOMContentLoaded',function(){
+            $('.js-popover').popover();
+        },false);
+    </script>
 </body>
 </html>
