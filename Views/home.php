8c9755ec07d970bf6e99681c9b77abc8bac5328c
syoki.isihira <zenkoku33@gmail.com>
2022-01-24 17:39:20 +0900

    ログイン機能

--- Views/home.php
+++ Views/home.php
@@ -1,45 +1,12 @@
-<?php
-// 設定関連を読み込む
-include_once('../config.php');
-// 便利な関数を読み込む
-include_once('../util.php');
-
-///////////////////////////////////////
-// ツイート一覧
-///////////////////////////////////////
-$view_tweets = [
-    [
-        'user_id' =>1,
-        'user_name' =>'taro',
-        'user_nickname' =>'太郎',
-        'user_image_name' =>'sample-person.jpg',
-        'tweet_body' =>'今プログラミングをしています。',
-        'tweet_image_name' =>null,
-        'tweet_created_at' =>'2021-12-01 14:00:00',
-        'like_id' =>null,
-        'like_count' =>0,
-    ],
-    [
-        'user_id' =>2,
-        'user_name' =>'jiro',
-        'user_nickname' =>'次郎',
-        'user_image_name' =>null,
-        'tweet_body' =>'コワーキングスペースをオープンしました!',
-        'tweet_image_name' =>'sample-post.jpg',
-        'tweet_created_at' =>'2021-03-14 14:00:00',
-        'like_id' =>1,
-        'like_count' =>1,
-    ]
-];
-
-?>
 <!DOCTYPE html>
 <html lang="ja">
+ 
 <head>
     <?php include_once('../Views/common/head.php'); ?>
     <title>ホーム画面 / Twitterクローン</title>
     <meta name="description" content="ホーム画面です">
 </head>
+ 
 <body class="home">
     <div class="container">
         <?php include_once('../Views/common/side.php'); ?>
@@ -47,7 +14,8 @@ $view_tweets = [
             <div class="main-header">
                 <h1>ホーム</h1>
             </div>
-            <!-- つぶやき投稿エリア　-->
+ 
+            <!-- つぶやき投稿エリア -->
             <div class="tweet-post">
                 <div class="my-icon">
                     <img src="<?php echo HOME_URL; ?>Views/img_uploaded/user/sample-person.jpg" alt="">
@@ -64,21 +32,23 @@ $view_tweets = [
                     </form>
                 </div>
             </div>
+ 
             <!-- 仕切りエリア -->
             <div class="ditch"></div>
-
+ 
             <!-- つぶやき一覧エリア -->
             <?php if (empty($view_tweets)) : ?>
-                <P class="p-3">ツイートがありません</P>
-            <?php else: ?>
-            <div class="tweet-list">
-                <?php foreach($view_tweets as $view_tweet): ?>
-                    <?php include('../Views/common/tweet.php');?>
-                <?php endforeach; ?>
-            </div>
+                <p class="p-3">ツイートがありません</p>
+            <?php else : ?>
+                <div class="tweet-list">
+                    <?php foreach ($view_tweets as $view_tweet) : ?>
+                        <?php include('../Views/common/tweet.php'); ?>
+                    <?php endforeach; ?>
+                </div>
             <?php endif; ?>
         </div>
-    </div> 
+    </div>
     <?php include_once('../Views/common/foot.php'); ?>
 </body>
+ 
 </html>
--- Views/sign-in.php
+++ Views/sign-in.php
@@ -1,23 +1,26 @@
-<?php
-// 設定関連を読み込む
-include_once('../config.php');
-// 便利な関数を読み込む
-include_once('../util.php');
-?>
 <!DOCTYPE html>
 <html lang="ja">
+ 
 <head>
     <?php include_once('../Views/common/head.php'); ?>
     <title>ログイン画面 / Twitterクローン</title>
     <meta name="description" content="ログイン画面です">
 </head>
+ 
 <body class="signup text-center">
     <main class="form-signup">
         <form action="sign-in.php" method="post">
-            <img src="<?php echo HOME_URL;?>Views/img/logo-white.svg" alt="" class="logo-white">
+            <img src="<?php echo HOME_URL; ?>Views/img/logo-white.svg" alt="" class="logo-white">
             <h1>Twitterクローンにログイン</h1>
-            <input type="email" class="form-control" name="email" placeholder="メールアドレス"  required autofocus>
-            <input type="password" class="form-control" name="password" placeholder="パスワード" minlength="4" maxlength="50" required>
+ 
+            <?php if (isset($view_try_login_result) && $view_try_login_result === false) : ?>
+                <div class="alert alert-warning text-sm" role="alert">
+                    ログインに失敗しました。メールアドレス、パスワードが正しいかご確認ください。
+                </div>
+            <?php endif; ?>
+ 
+            <input type="email" class="form-control" name="email" placeholder="メールアドレス" required autofocus>
+            <input type="password" class="form-control" name="password" placeholder="パスワード" required>
             <button class="w-100 btn btn-lg" type="submit">ログイン</button>
             <p class="mt-3 mb-2"><a href="sign-up.php">会員登録する</a></p>
             <p class="mt-2 mb-3 text-muted">&copy; 2021</p>
@@ -25,4 +28,5 @@ include_once('../util.php');
     </main>
     <?php include_once('../Views/common/foot.php'); ?>
 </body>
+ 
 </html>
