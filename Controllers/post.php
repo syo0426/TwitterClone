2c82ab5e0a52552e9a48a41b5b4c2166dd087f45
syoki.isihira <zenkoku33@gmail.com>
2022-02-01 16:38:06 +0900

    ホーム機能

--- Controllers/post.php
+++ Controllers/post.php
@@ -7,31 +7,31 @@
 include_once '../config.php';
 // 便利な関数を読み込み
 include_once '../util.php';
-
+ 
 // ツイートデータ操作モデルを読み込む
 include_once '../Models/tweets.php';
-
-//　ログインチェック
+ 
+// ログインチェック
 $user = getUserSession();
 if (!$user) {
     // ログインしていない
     header('Location: ' . HOME_URL . 'Controllers/sign-in.php');
     exit;
 }
-
+ 
 // ツイートがある場合
 if (isset($_POST['body'])) {
     $image_name = null;
     if (isset($_FILES['image']) && is_uploaded_file($_FILES['image']['tmp_name'])) {
-        $image_name = uploadImage($user, $_FILES['image'], 'tweet'); 
+        $image_name = uploadImage($user, $_FILES['image'], 'tweet');
     }
-
+ 
     $data = [
         'user_id' => $user['id'],
         'body' => $_POST['body'],
         'image_name' => $image_name,
     ];
-
+ 
     // つぶやき投稿
     if (createTweet($data)) {
         // ホーム画面に遷移
@@ -39,9 +39,9 @@ if (isset($_POST['body'])) {
         exit;
     }
 }
-
-//表示用の変数
+ 
+// 表示用の変数
 $view_user = $user;
-
-//画面表示
+ 
+// 画面表示
 include_once '../Views/post.php';
