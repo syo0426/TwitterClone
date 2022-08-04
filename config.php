59fed93f85cbe815a19e3830024acf4e50490e99
syoki.isihira <zenkoku33@gmail.com>
2022-08-04 16:52:43 +0900

    SQL等の変更

--- config.php
+++ config.php
@@ -4,9 +4,9 @@ ini_set('display_errors', 1);
 // 日本時間にする
 date_default_timezone_set('Asia/Tokyo');
 // URL/ディレクトリ設定
-define('HOME_URL', '/TwitterClone/');
+define('HOME_URL', '');
 // データベースの接続情報
-define('DB_HOST','localhost');
-define('DB_USER','root');
-define('DB_PASSWORD','root');
-define('DB_NAME','twitter_clone');
+define('DB_HOST','mysql206.phy.lolipop.lan');
+define('DB_USER','LAA1449406');
+define('DB_PASSWORD','zenkoku33');
+define('DB_NAME','LAA1449406-twitterclone');
---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+/* リダイレクトさせたい時は、このように記述します。 */
+header('Location:http://twitterclone.syokiii.com/Controllers/sign-in.php');
+exit;
+?>
