2ee689099662abc59412eb5b99f6c850406e5791
syoki.isihira <zenkoku33@gmail.com>
2022-08-11 17:29:07 +0900

    config.phpの修正

--- config.php
+++ config.php
@@ -4,7 +4,7 @@ ini_set('display_errors', 1);
 // 日本時間にする
 date_default_timezone_set('Asia/Tokyo');
 // URL/ディレクトリ設定
-define('HOME_URL', '');
+define('HOME_URL', '/');
 // データベースの接続情報
 define('DB_HOST','mysql206.phy.lolipop.lan');
 define('DB_USER','LAA1449406');
